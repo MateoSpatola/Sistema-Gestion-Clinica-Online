2a11df9b8b4fa1d6c398714c54c1a1460f5f6088
MateoSpatola <mateospatola@hotmail.com>
2024-10-29 23:46:48 -0300

    Creacion y ruteo de las paginas de inicio, de registro y de ingreso.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', loadComponent: () => import('./pages/inicio/inicio.component').then((m) => m.InicioComponent) },
+    { path: 'registro', loadComponent: () => import('./pages/auth/registro/registro.component').then((m) => m.RegistroComponent) },
+    { path: 'ingreso', loadComponent: () => import('./pages/auth/ingreso/ingreso.component').then((m) => m.IngresoComponent) },
+];
