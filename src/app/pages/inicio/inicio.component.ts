623622a2dd0b5937aa98f6770ce011e3d906f448
MateoSpatola <mateospatola@hotmail.com>
2024-11-12 20:42:21 -0300

    Modificacion acceso a boton ingreso y registro en inicio.

--- src/app/pages/inicio/inicio.component.ts
+++ src/app/pages/inicio/inicio.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
+import { Unsubscribe, User } from '@angular/fire/auth';
 
 @Component({
   selector: 'app-inicio',
@@ -11,5 +13,20 @@ import { RouterLink } from '@angular/router';
   styleUrl: './inicio.component.css'
 })
 export class InicioComponent {
+  private _authService = inject(AuthService);
 
+  private authSubscription?: Unsubscribe;
+  user?: User | null;
+
+  ngOnInit() {
+    this.authSubscription = this._authService.auth.onAuthStateChanged(user => {
+      this.user = user;
+    })
+  }
+
+  ngOnDestroy() {
+    if (this.authSubscription) {
+      this.authSubscription();
+    }
+  }
 }
